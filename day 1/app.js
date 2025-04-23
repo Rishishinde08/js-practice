c87ae07fd4d7841b32901cd57ff18e6812cb0439
Hrishikesh Shinde <rishikeshs2003@gmail.com>
2025-04-23 22:56:21 +0530

    day one finish

--- day 1/app.js
+++ day 1/app.js
@@ -75,3 +75,26 @@ case 10 :
 for(let i = 0; i<=10; i++){
     console.log(`i = ${i}`);
 }
+
+// alert and promt 
+
+// let instruct = alert("andar mat ja ");
+// let promts = prompt("enter the number");
+// console.log(promts);
+
+
+// stirng opration 
+
+// let pass = prompt(`Enter the password: `);
+// let NewPass = pass.trim();
+// console.log(NewPass); 
+
+let str = "  Hello JavaScript!  ";
+console.log(str.length); // 21
+console.log(str.trim()); // "Hello JavaScript!"
+console.log(str.toUpperCase()); // "  HELLO JAVASCRIPT!  "
+console.log(str.includes("Java")); // true
+console.log(str.slice(2, 7)); // "Hello"
+console.log(str.split(" ")); // ["", "", "Hello", "JavaScript!", "", ""]
+
+
