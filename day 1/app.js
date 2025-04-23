4d1b676eeea38b643b250614e599b10aecbc7c12
Hrishikesh Shinde <rishikeshs2003@gmail.com>
2025-04-23 22:24:04 +0530

    day one

---
+++ day 1/app.js
@@ -0,0 +1,55 @@
+
+
+// sum code 
+
+let a = 10;
+let b = 20;
+
+console.log(a+ " + " + b + " = " + (a+b));
+console.log(`sum of ${a} +  ${b} = ${a%b}`);
+console.log(`sum of ${a} +  ${b} = ${a-b}`);
+console.log(`sum of ${a} +  ${b} = ${a/b}`);
+console.log(`sum of ${a} +  ${b} = ${a*b}`);
+console.log(`sum of ${a} +  ${b} = ${a**b}`);
+
+
+// if else statement 
+
+let c = 10 ;
+let d = 10 ;
+
+
+if(c>d){
+    console.log("this is a true value");
+}else{
+    console.log("this is a false value");
+
+}
+
+
+if(null){
+    console.log("this is a true value");
+}else{
+    console.log("this is a false value");
+
+}
+
+if(NaN){
+    console.log("this is a true value");
+}else{
+    console.log("this is a false value");
+
+}
+
+if(""){
+    console.log("this is a true value");
+}else{
+    console.log("this is a false value");
+
+}
+
+if(0){
+    console.log("this is a true value");
+}else{
+    console.log("this is a false value");
+}
