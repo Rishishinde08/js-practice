b79835e16222498dece809fe987d9cd741ef6ed4
Hrishikesh Shinde <rishikeshs2003@gmail.com>
2025-04-26 09:40:20 +0530

    third-day-pattern-printing

---
+++ day 3/app.js
@@ -0,0 +1,63 @@
+
+
+
+console.log("pattern printing in js  ");
+
+for (let i = 0; i <= 5; i++) {
+
+    let row = "";
+    for (let j = 0; j <= i; j++) {
+        row += "* "
+    }
+
+    console.log(row);
+}
+
+console.log("Pattern printing in JS:");
+
+for (let i = 5; i >= 0; i--) {
+    let row = "   ";
+    for (let j = 0; j <= i; j++) {
+        row += " *";
+    }
+    console.log(row);
+}
+
+
+console.log("square pattern in JS");
+
+for (let i=0; i<4; i++){
+
+    let row = "";
+    for(let j=0; j< 5; j++){
+        row += " *";
+    }
+
+
+    console.log(row);
+}
+
+
+
+
+console.log("side trangle in js ");
+
+
+for(let i=0; i<=5; i++){
+
+
+    for(let k=0; k<=5; k++){
+
+       console.log(" ");
+    }
+
+    let row = "";
+
+for(let j=0; j<=i; j++){
+
+    row += " *";
+}
+
+    console.log(row);
+}
+
