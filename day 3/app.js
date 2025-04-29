bf56b8bf93e7971718289902a6fe03b2b5b938bf
Hrishikesh Shinde <rishikeshs2003@gmail.com>
2025-04-29 20:13:37 +0530

    day-10

--- day 3/app.js
+++ day 3/app.js
@@ -1,93 +1,93 @@
 
 
 
-// console.log("pattern printing in js  ");
+console.log("pattern printing in js  ");
 
-// for (let i = 0; i <= 5; i++) {
+for (let i = 0; i <= 5; i++) {
 
-//     let row = "";
-//     for (let j = 0; j <= i; j++) {
-//         row += "* "
-//     }
+    let row = "";
+    for (let j = 0; j <= i; j++) {
+        row += "* "
+    }
 
-//     console.log(row);
-// }
+    console.log(row);
+}
 
-// console.log("Pattern printing in JS:");
+console.log("Pattern printing in JS:");
 
-// for (let i = 5; i >= 0; i--) {
-//     let row = "   ";
-//     for (let j = 0; j <= i; j++) {
-//         row += " *";
-//     }
-//     console.log(row);
-// }
-
-
-// console.log("square pattern in JS");
-
-// for (let i=0; i<4; i++){
-
-//     let row = "";
-//     for(let j=0; j< 5; j++){
-//         row += " *";
-//     }
+for (let i = 5; i >= 0; i--) {
+    let row = "   ";
+    for (let j = 0; j <= i; j++) {
+        row += " *";
+    }
+    console.log(row);
+}
 
 
-//     console.log(row);
-// }
+console.log("square pattern in JS");
+
+for (let i=0; i<4; i++){
+
+    let row = "";
+    for(let j=0; j< 5; j++){
+        row += " *";
+    }
 
 
-
-
-// console.log("side trangle in js ");
-
-
-// for(let i=0; i<=5; i++){
-
-
-//     for(let k=0; k<=5; k++){
-
-//        console.log(" ");
-//     }
-
-//     let row = "";
-
-// for(let j=0; j<=i; j++){
-
-//     row += " *";
-// }
-
-//     console.log(row);
+    console.log(row);
 // }
 
 
-// // objects 
 
-// console.log("object Litral");
 
-// let student = {
-//    name: "rishi",
-//   rollNumber: 28,
-//   age : 21
-// };
+console.log("side trangle in js ");
 
-// const post ={
 
-//     like : 20,
-//     Comment : "very nice bro",
-//     tags : ["@rishi" , "@rishikes"]
+for(let i=0; i<=5; i++){
 
-// }
 
-// let result = post.like;
+    for(let k=0; k<=5; k++){
 
-// // console.name(student.name);
-// console.log(result);
+       console.log(" ");
+    }
 
-// console.log(post["like"]);
+    let row = "";
 
-// student["name"];
+for(let j=0; j<=i; j++){
+
+    row += " *";
+}
+
+    console.log(row);
+}
+
+
+// objects 
+
+console.log("object Litral");
+
+let student = {
+   name: "rishi",
+  rollNumber: 28,
+  age : 21
+};
+
+const post ={
+
+    like : 20,
+    Comment : "very nice bro",
+    tags : ["@rishi" , "@rishikes"]
+
+}
+
+let result = post.like;
+
+// console.name(student.name);
+console.log(result);
+
+console.log(post["like"]);
+
+student["name"];
 
 
 
